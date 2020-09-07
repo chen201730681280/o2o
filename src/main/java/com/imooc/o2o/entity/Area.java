538e7e652a56f19451159e4959871ea271a306a9
chenyx <774503029@qq.com>
2020-09-07 17:38:24 +0800

    创建表
    area--区域表
    personinfo--用户信息
    tb_wechat_auth 微信账号
    tb_local_auth本地账号
    dengdeng

--- src/main/java/com/imooc/o2o/entity/Area.java
+++ src/main/java/com/imooc/o2o/entity/Area.java
@@ -11,6 +11,46 @@ public class Area {
     private  Integer priority;
     //createTime创建时间
     private Date createTime;
-    //更新
+    //更新时间
     private Date lastEditTime;
+
+    public Integer getAreaId() {
+        return areaId;
+    }
+
+    public void setAreaId(Integer areaId) {
+        this.areaId = areaId;
+    }
+
+    public String getAreaName() {
+        return areaName;
+    }
+
+    public void setAreaName(String areaName) {
+        this.areaName = areaName;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getLastEditTime() {
+        return lastEditTime;
+    }
+
+    public void setLastEditTime(Date lastEditTime) {
+        this.lastEditTime = lastEditTime;
+    }
 }
